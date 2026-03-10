9736a9447580c23ec3df1cfb3cb3ad6344045f89
Viktor226 <vanistratenko.a.5@gmail.com>
2026-03-10 14:09:42 +0200

    Fix input fields and improve book management

--- app.py
+++ app.py
@@ -1,67 +1,86 @@
 import streamlit as st
+
+
 st.title("Библиотека")
+
+
 if "books" not in st.session_state:
-  st.session_state.books = []
+    st.session_state.books = []
 
+# Секция за добавяне на книга
 st.header("Добави книга")
-title = st.text_input("zaglawie")
-author= st.text_input("awtor")
-price = st.text_input("cena", min_value=0.0)
+title = st.text_input("Заглавие")
+author = st.text_input("Автор")
+price = st.number_input("Цена")  
+
 if st.button("Добави книга"):
+    if title and author and price:  
+        book = {
+            "title": title,
+            "author": author,
+            "price": price,
+        }
+        st.session_state.books.append(book)  
+        st.success("Книгата е добавена")
+    else:
+        st.warning("Моля, попълнете всички полета")
 
- book = {
-  "title": title,
-  "author": author,
-  "price" : price,
-}
-
-st.session_stete.books.append(book)
-st.success("Книгата е добавена")
- 
 
 if st.button("Покажи всички книги"):
-  st.write("няма добавени книги")
-else:
-  for book in st.session_stete.books:
-    st.write("Заглавие:", book["title"])
-    st.write("Автор :", book["author"])
-    st.write("Цена:", book["price"])
-    st.write("---------------------------")
+    if len(st.session_state.books) == 0:  
+        st.write("Няма добавени книги")
+    else:
+        for book in st.session_state.books:  
+            st.write("Заглавие:", book["title"])
+            st.write("Автор:", book["author"])
+            st.write("Цена:", book["price"])
+            st.write("---------------------------")
+
+
 st.header("Търсене по автор")
-search_author = st.text_input("Въведи автор, които искаш да намериш")
+search_author = st.text_input("Въведи автор, който искаш да намериш")
+
 if st.button("Търси по автор"):
- found = false
- for book in st.session_state.books:
-  if book["author"] == search_author:
-   st.write(book)
-   found = True
+    found = False  
+    for book in st.session_state.books:
+        if book["author"] == search_author:
+            st.write("Заглавие:", book["title"])
+            st.write("Автор:", book["author"])
+            st.write("Цена:", book["price"])
+            st.write("---------------------------")
+            found = True
+    if not found:  
+        st.write("Няма намерени книги от този автор")
 
 
-st.header = ("Търси по заглавие")
+st.header("Търсене по заглавие")  
 search_title = st.text_input("Въведи заглавие")
 
 if st.button("Търси по заглавие"):
+    found = False  
+    for book in st.session_state.books:
+        if book["title"] == search_title:
+            st.write("Заглавие:", book["title"])
+            st.write("Автор:", book["author"])
+            st.write("Цена:", book["price"])
+            st.write("---------------------------")
+            found = True
+    if not found:  
+        st.write("Книга не е намерена")
 
- found = False
- for book in st.session_state.books:
-   if book ["title"] == search_title:
-     st.write(book, "е намерена")
-     found = True
 
-if found == false:
-  st.write("книга не е намерена")
-
-if st.button("Покайи наи-евтина книга"):
- if len(session_state.books) == 0:
-  st.error("няма книги")
-
-else :
-  cheapest = session_state.books[0]
-
-for book in session_state.books:
-  if book["price"] < cheapest["price"]:
-    chapest = book["price"]
-  st.write(cheapest)
+if st.button("Покажи най-евтина книга"):
+    if len(st.session_state.books) == 0:  
+        st.error("Няма книги")
+    else:
+        cheapest = st.session_state.books[0]
+        for book in st.session_state.books:
+            if book["price"] < cheapest["price"]:  
+                cheapest = book  
+        st.write("Най-евтината книга е:")
+        st.write("Заглавие:", cheapest["title"])
+        st.write("Автор:", cheapest["author"])
+        st.write("Цена:", cheapest["price"])
 
 
 
